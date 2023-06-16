1fa18d4dfc6dee12b8103c4e512336608d4030e7
Дмитрий <dimualdos@gmail.com>
2023-06-16 17:03:40 +0000

    код по уроку

--- myPolyfills/index.js
+++ myPolyfills/index.js
@@ -55,6 +55,7 @@ module.exports = function () {
                     }
                 };
                 return result;
+                
             }
             return flatten(this, depth);
 
